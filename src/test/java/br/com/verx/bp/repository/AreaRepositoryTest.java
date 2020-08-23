8de8b934c86ef5282ffa40d1a975d3594374dbb3
Bruno Vitalino <brunovitalino@hotmail.com>
2020-08-23 12:14:08 -0300

    Vinculed test profile to Area tests

--- src/test/java/br/com/verx/bp/repository/AreaRepositoryTest.java
+++ src/test/java/br/com/verx/bp/repository/AreaRepositoryTest.java
@@ -11,11 +11,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import br.com.verx.bp.model.Area;
 
 @SpringBootTest
 @Transactional
+@ActiveProfiles("test")
 public class AreaRepositoryTest {
 	
 	private Area area;
