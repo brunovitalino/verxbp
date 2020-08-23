71ea8321a6ff0bfab6f9e33c4c377d94bf2054cb
Bruno Vitalino <brunovitalino@hotmail.com>
2020-08-23 12:21:51 -0300

    Changing controller type to rest

--- src/main/java/br/com/verx/bp/controller/ConfigurationController.java
+++ src/main/java/br/com/verx/bp/controller/ConfigurationController.java
@@ -1,16 +1,16 @@
 package br.com.verx.bp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
 import br.com.verx.bp.model.User;
 import br.com.verx.bp.repository.UserRepository;
 
-@Controller
+@RestController
 public class ConfigurationController {
 	
 	@Autowired
