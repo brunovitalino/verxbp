4d19fe3f733ece484f6ab4e48cbd6bc5264acaa0
Bruno Vitalino <brunovitalino@hotmail.com>
2020-08-24 06:13:45 -0300

    Changed documentation config to permit tests using tokens

--- src/main/java/br/com/verx/bp/config/swagger/SwaggerConfiguration.java
+++ src/main/java/br/com/verx/bp/config/swagger/SwaggerConfiguration.java
@@ -1,11 +1,15 @@
 package br.com.verx.bp.config.swagger;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import br.com.verx.bp.model.User;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -19,7 +23,16 @@ public class SwaggerConfiguration {
 				.apis(RequestHandlerSelectors.basePackage("br.com.verx.bp.controller"))
 				.paths(PathSelectors.ant("/**"))
 				.build()
-				.ignoredParameterTypes(User.class);
+				.ignoredParameterTypes(User.class)
+				.globalOperationParameters(
+                        Arrays.asList(
+                                new ParameterBuilder()
+                                    .name("Authorization")
+                                    .description("Header for JWT Token")
+                                    .modelRef(new ModelRef("string"))
+                                    .parameterType("header")
+                                    .required(false)
+                                    .build()));
 	}
 
 }
